fad6a5065ec82c31b52791ea17e87cbb0316fd42
Jubayed-A <learnigprograming@gmail.com>
2023-12-14 00:22:27 +0600

    code payment section added

--- app/src/main/java/com/example/pointofsale/OutputActivity.kt
+++ app/src/main/java/com/example/pointofsale/OutputActivity.kt
@@ -1,9 +1,14 @@
 package com.example.pointofsale
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -16,6 +21,53 @@ class OutputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_output)
+
+        // select payment method code here
+
+        // payment text code here
+        val payment = arrayOf(
+            "Bank System", "Cash On Delivery", "Mobile Banking"
+        )
+
+        val arrayAdapter = ArrayAdapter(this, R.layout.drop_down_item, payment)
+        val paymentList = findViewById<AutoCompleteTextView>(R.id.autoItem)
+        paymentList.setAdapter(arrayAdapter)
+
+        paymentList.setOnItemClickListener { parent, view, position, id ->
+            val selectedPayment = parent.getItemAtPosition(position).toString()
+//            Toast.makeText(this, paymentList.text, Toast.LENGTH_SHORT).show()
+            when(selectedPayment){
+                "Bank System" -> {
+                    // bank activity create
+                    val intentBank = Intent(this, BankActivity::class.java)
+                    startActivity(intentBank)
+                }
+                "Cash On Delivery" ->{
+                    // custom alert box show
+                    val alertDialog = AlertDialog.Builder(this)
+                        .setTitle("Bank System Payment")
+                        .setMessage("Custom Alert Dialog for Bank System Payment")
+                        .setPositiveButton("OK") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .create()
+
+                    alertDialog.show()
+                }
+                "Mobile Banking" ->{
+                    val intentMobile = Intent(this, MobileActivity::class.java)
+                    startActivity(intentMobile)
+                }
+                else ->{
+                    Toast.makeText(this, "Selected payment: $selectedPayment", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+
+
+
+
 
         val name = intent.getStringArrayListExtra("itemList")
         val qty = intent.getStringArrayListExtra("itemQty")
