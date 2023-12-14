3681c8be51050ade69be461697e8ad861eb54280
Jubayed-A <learnigprograming@gmail.com>
2023-12-15 01:58:28 +0600

    smart phone activity added

---
+++ app/src/main/java/com/example/pointofsale/SmartPhone_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.pointofsale
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SmartPhone_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_smart_phone)
+    }
+}
