32222a3844ce03d7bb484077ef66afd0ac268902
Jubayed-A <learnigprograming@gmail.com>
2024-02-20 23:47:52 +0600

    some file updated

--- Foisal_Sir_Andoid/PicassolibraryImport/app/build.gradle.kts
+++ Foisal_Sir_Andoid/PicassolibraryImport/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pointofsale"
+    namespace = "com.example.picassolibraryimport"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.pointofsale"
+        applicationId = "com.example.picassolibraryimport"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -37,7 +37,6 @@ android {
 
 dependencies {
 
-    //noinspection GradleCompatible
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -45,11 +44,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
-    implementation ("com.github.f0ris.sweetalert:library:1.5.1")
-
-    // Loading button
-    implementation("br.com.simplepass:loading-button-android:2.2.0")
-
 }
